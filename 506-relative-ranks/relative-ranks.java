7a5d1f8fc395807c94ebaa339f07738d859b9adb
Videeksha <161011302+Videeksha22@users.noreply.github.com>
2025-12-13 17:18:39 +0530

    Time: 10 ms (60.51%) | Memory: 47.5 MB (31.85%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,20 +1,20 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        int n=score.length;
+        int n = score.length;
         int[][] sortedPairs = new int[n][2];
-        for (int i = 0; i < n; i++) {
+        for (int i=0; i<n; i++) {
             sortedPairs[i][0] = score[i];
             sortedPairs[i][1] = i;
         }
-        Arrays.sort(sortedPairs, (x, y) -> y[0]-x[0]);
-        String[] ans = new String[n];
-        for(int i=0; i<n; i++){
+        Arrays.sort(sortedPairs, (a, b) -> b[0] - a[0]);
+        String[] result = new String[n];
+        for (int i=0; i<n; i++) {
             int index = sortedPairs[i][1];
-            if (i == 0) ans[index] = "Gold Medal";
-            else if (i == 1) ans[index] = "Silver Medal";
-            else if (i == 2) ans[index] = "Bronze Medal";
-            else ans[index] = String.valueOf(i + 1);
+            if (i == 0) result[index] = "Gold Medal";
+            else if (i == 1) result[index] = "Silver Medal";
+            else if (i == 2) result[index] = "Bronze Medal";
+            else result[index] = String.valueOf(i + 1);
         }
-        return ans;
+        return result;
     }
 }
